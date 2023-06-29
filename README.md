618894b881ffd97dec4a526a754432874f5c22d6
unknown <1065637910@qq.com>
2023-06-29 16:32:08 +0800

    1

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # duobuhui.github.io
-Web 学习# duobuhui.github.io
-# duobuhui.github.io
-# duobuhui.github.io
+Web 学习
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/mixfold2-duobuhui.png') {
+      myImage.setAttribute('src', 'images/mixfold2-hanser.png');
+    } else {
+      myImage.setAttribute('src', 'images/mixfold2-duobuhui.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = '多不会 酷毙了，' + myName;
+    }
+  }  
+myButton.onclick = function() {
+    setUserName();
+ }
